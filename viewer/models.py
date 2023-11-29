76fae988ef66089c5eff9f81d69d52175ea365ce
Matúš Kočik <matuskocik@gmail.com>
2023-11-29 20:30:20 +0100

    small change in models.py

--- viewer/models.py
+++ viewer/models.py
@@ -9,6 +9,9 @@ class Country(Model):
     
     class Meta:
         verbose_name_plural = "Countries"
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 class Genre(Model):
     name = CharField(max_length=128, null=False, blank=False) # CharField => VARCHAR
@@ -21,7 +24,13 @@ class Person(Model):
     last_name = CharField(max_length=32, null=False, blank=False)
     birth_date = DateField(null=True, blank=True)
     biography = TextField(null=True, blank=True)
-
+    
+    class Meta:
+        ordering = ["last_name", "first_name"]
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
 class Movie(Model):
     title_orig = CharField(max_length=128, null=False, blank=False)
     title_cz = CharField(max_length=128, null=True, blank=True)
@@ -34,17 +43,31 @@ class Movie(Model):
     video = CharField(max_length=128, null=True, blank=True)
     description = TextField(null=True, blank=True)
 
+    def __str__(self) -> str:
+        # TODO: pokial existuje nazov cesky alebo slovensky tak zobrazit a ak ne tak ENG + do zatvorky rok
+        # Pr: Forrest Gump (1994)
+        return f"{self.title_orig}"
+
 class Rating(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
     user = ForeignKey(User, null=True, on_delete=SET_NULL)
     rating = IntegerField(null=False, blank=False)
     
+"""     def __str__(self) -> str:
+        return f"{self.name}" """
+    
 class Comment(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING)
     user = ForeignKey(User, null=True, on_delete=SET_NULL)
     comment = TextField(null=False, blank=False)
+    
+"""     def __str__(self) -> str:
+        return f"{self.name}" """
 
 class Image(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
     url = CharField(max_length=128, null=False, blank=False)
     description = TextField()
+    
+"""     def __str__(self) -> str:
+        return f"{self.name}" """
