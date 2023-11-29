4367ecc9b6d147cc594fbc3d6ac53927f1f42526
Matúš Kočik <matuskocik@gmail.com>
2023-11-29 08:09:02 +0100

    add new view hello2 with 1 argument

--- viewer/views.py
+++ viewer/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello(request):
-    return HttpResponse ("Hello world!")
+    return HttpResponse("Hello world!")
+
+def hello2(request, s):
+    return HttpResponse(f"Hello {s} world!")
