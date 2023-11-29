847da99158d7d1a5f70fb0c4b4f297b4fce5fdff
Matúš Kočik <matuskocik@gmail.com>
2023-11-29 18:14:27 +0100

    add first table, migrate

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models import Model, CharField
 
 # Create your models here.
+
+class Genre(Model):
+    name = CharField(max_length=128) # CharField => VARCHAR
+
+
