881011d40b43bd5af1a26ff43bcd40e19febd6b1
Matúš Kočik <matuskocik@gmail.com>
2023-12-06 17:36:38 +0100

    update views

--- viewer/views.py
+++ viewer/views.py
@@ -38,10 +38,40 @@ def index(request):
 
 
 def movies(request):
+    g = request.GET.get("genre", "")
+    genres = Genre.objects.all()
+    if g != "" and c != "":
+        g = int(g)
+        c = int(c)
+        if Genre.objects.filter(id=g).exists() and Country.objects.filter(id=c).exists():
+            genre = Genre.objects.get(id=g)
+            country = Country.objects.get(id=c)
+            movie_list = Movie.objects.filter(genres = genre, countries = country)
+            context = {"movies": genre.movies_of_genre.all(), "genres": genres, "filtered_by": f"podla zanru{genre} a zeme {movies_by_country}"}
+            return render(request, "movies.html", context)
+    if g != "":
+        g = int(g)
+        if Genre.objects.filter(id=g).exists():          
+            genre = Genre.objects.get(id=g)
+            context = {"movies": genre.movies_of_genre.all(), "genres": genres, "filtered_by": f"podla zanru{genre}"}
+            return render(request, "movies.html", context)
+        else:
+            context = {"movies": [], "genres": genres, "filtered_by": ""}
+            return render(request, "movies.html", context)
+    c = request.GET.get("country", "")
+    countries = Country.objects.all()
+    if c != "":
+        c = int(c)
+        if Country.objects.filter(id=c).exists():
+            country = Country.objects.get(id=c)
+            context = {"movies": country.movies_in_country.all(), "genres": genres, "filtered_by": f"podla zeme{country}"}
+            return render(request, "movies.html", context)
+        else:
+            context = {"movies": [], "genres": genres, "filtered_by": ""}
+            return render(request, "movies.html", context)
     movies_list = Movie.objects.all()
     genres_list = Genre.objects.all()
-    countries_list = Country.objects.all()
-    context = {"movies": movies_list, "genres": genres_list, "countries": countries_list}
+    context = {"movies": movies_list, "genres": genres_list, "filtered_by": ""}
     return render(request, "movies.html", context)
 
 
