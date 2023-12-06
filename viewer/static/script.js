4ae099f25bd34469d590f0a11d5da816f16ec2e4
Matúš Kočik <matuskocik@gmail.com>
2023-12-06 08:57:32 +0100

    change css

--- viewer/static/script.js
+++ viewer/static/script.js
@@ -1,11 +1,11 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copywright-text").textContent = `© created by Matúš Kočik, ${currentYear}`;
 
-document.querySelectorAll('nav a').forEach(function(link) {
+/* document.querySelectorAll('nav a').forEach(function(link) {
     if (window.location.href.includes(link.getAttribute('href'))) {
         link.classList.add('active');
     }
-})
+}) */
 
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('open');
